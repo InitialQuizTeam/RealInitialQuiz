c7ad94fc03498c1ada4dbb11b14cab171950e884
ckk914 <ckk9114@naver.com>
2024-05-02 17:36:58 +0900

    # 메인 디자인 # 게임 종료 화면 수정

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,22 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
+    public static final String RESET = "\u001B[0m";
+    public static final String FONT_BLACK = "\u001B[30m";
+    public static final String FONT_RED = "\u001B[31m";
+    public static final String FONT_GREEN = "\u001B[32m";
+    public static final String FONT_BLUE = "\u001B[34m";
+    public static final String FONT_PURPLE = "\u001B[35m";
+    public static final String FONT_CYAN = "\u001B[36m";
+    public static final String FONT_WHITE = "\u001B[37m";
+    public static final String BACKGROUND_BLACK = "\u001B[40m";
+    public static final String BACKGROUND_RED = "\u001B[41m";
+    public static final String BACKGROUND_GREEN = "\u001B[42m";
+    public static final String BACKGROUND_YELLOW = "\u001B[43m";
+    public static final String BACKGROUND_BLUE = "\u001B[44m";
+    public static final String BACKGROUND_PURPLE = "\u001B[45m";
+    public static final String BACKGROUND_CYAN = "\u001B[46m";
+    public static final String BACKGROUND_WHITE = "\u001B[47m";
     //숫자인지 판별
     public static boolean isInteger(String input) {
         try {
@@ -23,7 +39,7 @@ public class Main {
 
 
         while (true) {
-            System.out.println("☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎");
+            System.out.println(FONT_CYAN+"☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎");
             System.out.println("          초성 맞추기 게임");
             System.out.println("☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎☁︎");
             System.out.println("☁︎        1. 회원가입                    ");
